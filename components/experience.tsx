1e9a0a46b8957d31e05480a45c65b1550d312886
saleem.office <badusha8@hotmail.com>
2024-02-21 16:21:31 +0300

    experience completed

--- components/experience.tsx
+++ components/experience.tsx
@@ -28,6 +28,22 @@ export default function Experience() {
               <React.Fragment key={index}>
                 <VerticalTimelineElement
                   visible={true}
+                  contentStyle={{
+                    background:"#f3f4f6",
+                    boxShadow:"none",
+                    border:"1px solid rgba(0,0,0,0.05)",
+                    textAlign:"left",
+                    padding:"1.3rem 2rem"
+                  }}
+                  contentArrowStyle={{
+                    borderRight:"0.4rem solid #9ca3af",
+                  }}
+                  date={item.date}
+                  icon={item.icon}
+                  iconStyle={{
+                    background:"white",
+                    fontSize:"1.5rem"
+                  }}
                 >
                   <h3 className="font-semibold capitalize">{item.title}</h3>
                   <p className="font-normal !mt-0">{item.location}</p>
