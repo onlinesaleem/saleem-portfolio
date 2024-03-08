a937ccf6fb9ffbbec8b7f6bdf5c7ce417a91ca56
onlinesaleem <onlinesaleem@gmail.com>
2024-03-08 08:18:38 +0300

    updated

--- lib/data.ts
+++ lib/data.ts
@@ -35,26 +35,26 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Programmer",
+    location: "Chennai, India",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "after master degree. I immediately found a job as a programmer.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2015",
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "Najran,Saudi Arabia",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I worked as a full-stack developer for 5 years . I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2016 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Riyadh, Saudi Arabia",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes Java, Spring Boot, React, Next.js, TypeScript, Tailwind, Prisma,Mysql  and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
     date: "2021 - present",
   },
@@ -62,23 +62,23 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "ticketing System",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      "I worked as a full-stack developer on this startup project for 2 years. Hospital industry.",
+    tags: ["Java", "Jsp", "mysql", "java -spring"],
     imageUrl: corpcommentImg,
   },
   {
-    title: "rmtDev",
+    title: "OVR system",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+      "full functional OVR system Hospital compilance with the JCI",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
     imageUrl: rmtdevImg,
   },
   {
-    title: "Word Analytics",
+    title: "Quality Management System",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+      "fully functional quality management system for healthcare industry ",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
     imageUrl: wordanalyticsImg,
   },
